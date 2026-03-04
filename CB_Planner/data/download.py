2c8eb20e53790625132ae4e3e0c6b4237e2bf60b
Karina Kenan Li <karinali20011210@gmail.com>
2026-03-04 04:18:01 +0000

    Create download.py

---
+++ CB_Planner/data/download.py
@@ -0,0 +1,12 @@
+# pretrained_url = https://huggingface.co/ChemBart/Pretrained-Full/resolve/main/ChemBart_Full4.pth
+# download and move to CB_Planner/functions/ChemBart/model/Pretrained-Full.pth
+
+# mcts_url = https://huggingface.co/ChemBart/Policy-Value/resolve/main/CB_MCTS.pth
+# download and move to ./CB_Planner/functions/ChemBart/model/Policy-Value.pth
+
+# temp_yield_url = https://huggingface.co/ChemBart/Temperature-Yield/resolve/main/temp_yield_bart.pth
+# download and move to ./CB_Planner/functions/ChemBart/model/Temperature-Yield.pth
+
+# basic_mol_url = https://huggingface.co/datasets/ChemBart/Basic-Mols/resolve/main/basic_mol.json
+# download and move to ./CB_Planner/functions/ChemBart/data/basic_mol.json
+
